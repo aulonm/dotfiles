5c04c170aa2f5c7adb55b770275c54c3af02c191
Aulon Mujaj <aulon.mujaj@gmail.com>
2020-12-30 15:22:33 +0100

    gain read/write access to spotify files

--- install.sh
+++ install.sh
@@ -233,4 +233,5 @@ echo -e "\n${start_green} Install spotify ${end_green}"
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
-
+sudo chmod a+wr /usr/share/spotify
+sudo chmod a+wr /usr/share/spotify/Apps -R
